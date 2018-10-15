7c9b0540823affdfd4a2fea0336342f7886562a1
Andrew Lucas <anlugueso@gmail.com>
2018-10-14 22:05:56 -0300

    Update ToDoApp.js

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -8,8 +8,10 @@ import store from './src/Store'
 
 class ToDoApp extends Component {
     render(){
-t            <View style={styles.container}>
+            <View style={styles.container}>
                 <Text> ToDo App</Text>
+                <Text> 1- learn redux.</Text>
+                <Text> 2- finish this app.</Text>
             </View>
         );
     }
